8a3d819c493a2d5871101b070b4c196139428a08
Muhammad Sheharyar Asif <88365949+Shehrryar@users.noreply.github.com>
2024-06-20 10:42:49 +0500

    up

--- app/Http/Controllers/admin/ShippingController.php
+++ app/Http/Controllers/admin/ShippingController.php
@@ -4,27 +4,37 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Country;
-
 use App\Models\Shipping;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class ShippingController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         $countries = Country::get();
         $data['countries'] = $countries;
+        $shipping_charges = Shipping::select('shipping_charges.*', 'countries.name')->leftJoin('countries', 'countries.id', 'shipping_charges.country_id')->get();
+        $data['shipping_charges'] = $shipping_charges;
         return view('admin.shipping.create', $data);
     }
 
-    public function store(Request $request){
-        $validator = Validator::make($request->all(),[
-            'country'=>'required',
-            'amount'=>'required|numeric'
+    public function store(Request $request)
+    {
+        $count = Shipping::where('country_id', $request->country)->count();
+        $validator = Validator::make($request->all(), [
+            'country' => 'required',
+            'amount' => 'required|numeric'
         ]);
 
-        if($validator->passes()){
-            
+        if ($validator->passes()) {
+            if ($count > 0) {
+                session()->flash('error', 'Shipping Already Exists');
+                return response()->json([
+                    'status' => true,
+                ]);
+            }
+
             $shipping = new Shipping();
             $shipping->country_id = $request->country;
             $shipping->amount = $request->amount;
@@ -34,11 +44,47 @@ class ShippingController extends Controller
             return response()->json([
                 'status' => true,
             ]);
-        }
-        else{
+        } else {
             return response()->json([
                 'status' => false,
-                'errors'=>$validator->errors()
+                'errors' => $validator->errors()
+            ]);
+        }
+    }
+    public function edit($id)
+    {
+        $countries = Country::get();
+
+        $shipping_charge = Shipping::find($id);
+
+        $data['countries'] = $countries;
+        $data['shipping_charge'] = $shipping_charge;
+        return view('admin.shipping.edit', $data);
+
+    }
+
+    public function update($id, Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'country' => 'required',
+            'amount' => 'required|numeric'
+        ]);
+
+        if ($validator->passes()) {
+
+            $shipping = Shipping::find($id);
+            $shipping->country_id = $request->country;
+            $shipping->amount = $request->amount;
+            $shipping->save();
+            session()->flash('success', 'Shipping updated Successfully');
+
+            return response()->json([
+                'status' => true,
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
             ]);
         }
     }
--- resources/views/admin/shipping/edit.blade.php
+++ resources/views/admin/shipping/edit.blade.php
@@ -31,7 +31,7 @@
                                     <option value="">Select a Country</option>
                                     @if ($countries->isNotEmpty())
                                         @foreach ($countries as $country)
-                                            <option value="{{$country->id}}">{{$country->name}}</option>
+                                            <option {{($shipping_charge->country_id == $country->id) ? 'selected' : '' }} value="{{$country->id}}">{{$country->name}}</option>
                                         @endforeach
                                     @endif
                                     <option value="rest_of_the_world">Rest of the World</option>
@@ -41,55 +41,22 @@
                         </div>
                         <div class="col-md-6">
 
-                            <input type="text" name="amount" id="amount" class="form-control" placeholder="amount">
+                            <input value="{{$shipping_charge->amount}}" type="text" name="amount" id="amount" class="form-control" placeholder="amount">
 
                         </div>
                         <div class="col-md-6">
-                            <button type="submit" id="getFormValuesButton" class="btn btn-primary">Create</button>
+                            <button type="submit" id="getFormValuesButton" class="btn btn-primary">Update</button>
                         </div>
                     </div>
                 </div>
             </div>
-        </form>
+        </form> 
 
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <table class="table table-striped">
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Amount</th>
-                                <th>Action</th>
-                            </tr>
-                            @if ($shipping_charges->isNotEmpty())
-                            @foreach ($shipping_charges as $charges)
-                            <tr>
-
-                                    <th>{{$charges->id}}</th>
-                                    <th>{{$charges->name}}</th>
-                                    <th>{{$charges->amount}}</th>
-                                    <th>
-                                        <a href="{{route('shipping.edit', $charges->id)}}" class="btn btn-primary" >Edit</a>
-                                        <a href="" class="btn btn-danger">Delete</a>
-                                    </th>
-
-                            </tr>
-                            @endforeach
-                            @endif
-                        </table>
-
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
     <!-- /.card -->
 </section>
 <!-- /.content -->
 @endsection
-
 @section('customjs') 
 <script>
 
@@ -97,8 +64,8 @@
         $("#shipping_from").submit(function (event) {
             event.preventDefault();
             $.ajax({
-                url: '{{route("shipping.store")}}',
-                type: 'post',
+                url: '{{route("shipping.update", $shipping_charge->id)}}',
+                type: 'put',
                 data: $(this).serializeArray(), // Use correct form ID
                 dataType: 'json', // 'datatype' should be 'dataType'
                 success: function (response) {
