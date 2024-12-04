dfb3241129bf7a3d532cd3c26e930808b7f2df2e
Muhammad Sheharyar Asif <88365949+Shehrryar@users.noreply.github.com>
2024-12-04 12:53:12 +0500

    up

--- app/Events/newTrae.php
+++ app/Events/newTrae.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class newTrae
+class newTrae implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
