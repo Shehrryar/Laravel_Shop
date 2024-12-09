48c252138c86a494d6c8a0c3023eb3709c851f05
Muhammad Sheharyar Asif <88365949+Shehrryar@users.noreply.github.com>
2024-12-09 10:04:59 +0500

    up

--- database/migrations/2024_11_21_183417_alter_users_table.php
+++ database/migrations/2024_11_21_183417_alter_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('address')->default(null);   // Total quantity added
+            $table->string('address')->nullable();   // Total quantity added
         });
     }
 
